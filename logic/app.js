167b2ddc472368b3f5cea1c5dd31d0e870c8a3ba
NikolayKertev <nikolaikertev@abv.bg>
2025-05-16 02:15:05 +0300

    Added some functions to the gallery and the menu.

--- logic/app.js
+++ logic/app.js
@@ -4,10 +4,20 @@ let imgWrapper = document.getElementById("fullImg");
 let close = document.querySelector("span");
 
 images.forEach((img, index) => {
-  img.addEventListener("click", () => {
-    openModel(`images/gallery/img${index+1}.jpg`);
+  img.addEventListener("click", (e) => {
+    openModel(`images/gallery/img${index + 1}.jpg`);
   });
-});
+  document.addEventListener('click', (e) => {
+    if (!img.contains(e.target)) {
+      console.log(e.target);
+      
+      if ((e.target.classList.contains("image") || (e.target.classList.contains("imageArea")))) {
+
+      }
+      else wrapper.style.display = "none";
+    }
+  })
+})
 
 close.addEventListener("click", () => (wrapper.style.display = "none"));
 
--- logic/menu.js
+++ logic/menu.js
@@ -5,6 +5,13 @@ menuWrapper.addEventListener('click', (e) => {
     openMenu();
 })
 
+document.addEventListener('click', (event) => {
+    if (!phoneNav.contains(event.target) && !menuWrapper.contains(event.target)) {
+        phoneNav.classList.add("hidden");
+        phoneNav.classList.remove("shown");
+    }
+});
+
 function openMenu() {
     if (phoneNav.classList.contains("shown")) {
         phoneNav.classList.add("hidden");
@@ -13,5 +20,5 @@ function openMenu() {
     else {
         phoneNav.classList.add("shown");
         phoneNav.classList.remove("hidden");
-    }    
+    }
 }
